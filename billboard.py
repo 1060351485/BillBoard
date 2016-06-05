cedc8ff9f567e4c368ce23975a0159d4e13d2c15
Apple <1060351485@qq.com>
2016-06-05 18:54:32 +0800

    add simple urwid menu to ui

--- billboard.py
+++ billboard.py
@@ -14,6 +14,7 @@ class BillBoardPlayer(object):
     def __init__(self):
         self.palette = None
         self.main_loop = None
+        self.main = None
         self.choices = u'Chapman Cleese Gilliam Idle Jones Palin'.split()
 
         self._setup_ui()
@@ -27,12 +28,12 @@ class BillBoardPlayer(object):
             ('outside', '', '', '', 'g27', '#a06'),
             ('bg', '', '', '', 'g7', '#d06'), ]
 
-        main = urwid.Padding(self.menu(u'Pythons', self.choices), left=2, right=2)
-        top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-                            align='center', width=('relative', 60),
+        self.main = urwid.Padding(self.menu(u'Music List', self.choices), left=10, right=2)
+        top = urwid.Overlay(self.main, urwid.SolidFill(),
+                            align='left', width=('relative', 60),
                             valign='middle', height=('relative', 60),
                             min_width=20, min_height=9)
-        self.main_loop = urwid.MainLoop(top, palette=self.palette)
+        self.main_loop = urwid.MainLoop(top)#, palette=self.palette)
 
     def _setup_signals(self):
         pass
@@ -62,41 +63,16 @@ class BillBoardPlayer(object):
         response = urwid.Text([u'You chose ', choice, u'\n'])
         done = urwid.Button(u'Ok')
         urwid.connect_signal(done, 'click', self.exit_program)
-        main.original_widget = urwid.Filler(urwid.Pile([response,
+        self.main.original_widget = urwid.Filler(urwid.Pile([response,
                                                         urwid.AttrMap(done, None, focus_map='reversed')]))
 
     def start(self):
-        # def exit_on_q(key):
-        #     if key in ('q', 'Q'):
-        #         raise urwid.ExitMainLoop()
-        #
-        # placeholder = urwid.SolidFill()
-        # loop = urwid.MainLoop(placeholder, self.palette, unhandled_input=exit_on_q)
-        # loop.screen.set_terminal_properties(colors=256)
-        # loop.widget = urwid.AttrMap(placeholder, 'bg')
-        # loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
-        #
-        # div = urwid.Divider()
-        # outside = urwid.AttrMap(div, 'outside')
-        # inside = urwid.AttrMap(div, 'inside')
-        # txt1 = urwid.Text(('banner', u" song_first "), align='center')
-        # txt2 = urwid.Text(('banner', u" song_second "), align='center')
-        # streak = urwid.AttrMap(txt1, 'streak')
-        # pile = loop.widget.base_widget  # .base_widget skips the decorations
-        # for item in [outside, inside, streak, inside, outside]:
-        #     pile.contents.append((item, pile.options()))
-        #
-        # loop.run()
-
         self.main_loop.run()
 
 
 def main():
     player = BillBoardPlayer()
     player.start()
-
-
-# pass
 
 
 if __name__ == "__main__":
