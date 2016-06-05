785f2ccb22c01d6f4a1fba38b22c424c4a74ffe5
Apple <1060351485@qq.com>
2016-06-05 16:13:09 +0800

    明确了几个基本类

---
+++ API/netease_api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+"""
+
+网易云音乐API
+
+"""
+
+
+class NetEaseApi(object):
+    pass
---
+++ billboard.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+"""
+
+顶层文件
+
+"""
+
+
+class BillBoardPlayer(object):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
