7f9df45120bd918d51d7f2642feb8b6f52b874b5
Apple <1060351485@qq.com>
2016-06-07 23:05:01 +0800

    beautifulsoup4 and re, which is better(faster)?

--- spider.py
+++ spider.py
@@ -3,16 +3,52 @@
 
 """
 
-网易云音乐BillBoard周榜
+网易云音乐获取音乐 http://music.163.com/#/discover/toplist?id=60198
+BillBoard周榜获取榜单 http://www.billboard.com/charts/hot-100
+
 
 """
 
 import urllib2
+import re
+from bs4 import BeautifulSoup
 
-url = 'http://music.163.com/#/discover/toplist?id=60198'
+net_ease_url = 'http://music.163.com/#/discover/toplist?id=60198'
+billboard_url = 'http://www.billboard.com/charts/hot-100'
 
-request = urllib2.Request(url)
-response = urllib2.urlopen(request)
+# request = urllib2.Request(billboard_url)
+response = urllib2.urlopen(billboard_url)
 page = response.read()
 
-# print page
+soup = BeautifulSoup(page, 'html.parser')
+
+last_week = re.findall(r'Last\sWeek:\s([0-9 -]+)</', page)
+#song_list = re.findall(r'chart-row__song">(.*?)</h2', page.encode('utf-8'))
+#singer_list = re.findall(r'chart-row__artist.*?Artist\sName">([^0-9]*?)</a', page)
+
+# singer_list = soup.find_all(re.compile(r'chart-row__artist.*?Artist\sName">([^0-9]*?)</a'))
+
+song_list = re.findall(r'chart-row__song">(.*?)</h2.*?Artist\sName">([^0-9]*?)</a', page)
+
+print len(last_week)
+print len(song_list)
+# print len(singer_list)
+
+
+def print_rank(this_rank, last_week):
+    if not last_week.isalnum():
+        return '--',
+    last_rank = int(last_week)
+    if last_rank == this_rank:
+        return '--',
+    elif last_rank > this_rank:
+        return 'down:' + str(last_rank - this_rank),
+    else:
+        return 'up:' + str(this_rank - last_rank),
+
+
+for i in range(len(song_list)):
+    # print '%-4d' % (i + 1),
+    # print '%-10s' % print_rank(i + 1, last_week[i]),
+    # print '%-30s' % song_list[i],
+    print '%-30s' % singer_list[i].strip()
