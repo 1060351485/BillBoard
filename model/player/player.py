2358bba9ac2c29760ac798b16914235de7478ba1
Apple <1060351485@qq.com>
2016-06-08 10:38:52 +0800

    try to finish to BillBoardSpider class

---
+++ model/player/player.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+# """
+#
+# 音乐播放
+#
+# """
+
+import subprocess
+import Queue
+import platform
+import os
+
+
+class Player(object):
+    def __init__(self):
+        self.is_playing = False
+        self.is_quit = True
+        self.current_playing = None
+        self.loop_one_song = False
+        self.loop_all = False
+        self.subprocess = None
+        self.music_url = '/Users/apple/PycharmProjects/BillBoard/'
+        self.queue = Queue.Queue()
+        self.platfrom = platform.system()
+        self.error_handle = open(self.music_url + 'model/player/' + 'tmp.log', 'wt')
+        # print self.platfrom
+        # print platform.release()
+
+    def play(self, name):
+        if self.is_playing:
+            self.subprocess.kill()
+        if self.platfrom == 'Darwin':
+            self.subprocess = subprocess.Popen(['mplayer', '-really-quiet', self.music_url + name],
+                                               stdin=subprocess.PIPE,
+                                               stderr=self.error_handle)
+        self.is_playing = True
+        self.current_playing = name
+
+    # def send_command(self):
+    #     pass
+
+    def stop(self):
+        if self.subprocess:
+            self.subprocess.kill()
+        self.is_playing = False
+
+    def pause(self):
+        self.subprocess.stdin.write(u'p')
+
+    def mute(self):
+        self.subprocess.stdin.write(u'm')
+
+    def volume_up(self):
+        self.subprocess.stdin.write(u'0')
+
+    def volume_down(self):
+        self.subprocess.stdin.write(u'9')
+
+    def close(self):
+        pass
+
+    def next(self):
+        pass
+
+    def quit(self):
+        self.stop()
+        self.is_quit = True
+        subprocess.call('echo -e "\033[?25h";clear', shell=True)
